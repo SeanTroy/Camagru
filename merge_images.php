fedb7c22e94ee12f96a15d43b807c255885984a7
Pekka Lehtikangas <plehtika@student.hive.fi>
2022-07-25 17:46:02 +0300

    php formatting

--- merge_images.php
+++ merge_images.php
@@ -3,41 +3,39 @@ session_start();
 require_once 'config/newpdo.php';
 
 if (isset($_SESSION['user_id']) && isset($_POST['new_image']) && isset($_POST['sticker'])) {
-// Create image instances
+	// Create image instances
 
-$image_url = $_POST['new_image'];
-$image_url = preg_replace('/^data:image\/jpeg;base64,/', '', $image_url);
-$image_url = str_replace(' ', '+', $image_url);
-$image = base64_decode($image_url);
+	$image_url = $_POST['new_image'];
+	$image_url = preg_replace('/^data:image\/jpeg;base64,/', '', $image_url);
+	$image_url = str_replace(' ', '+', $image_url);
+	$image = base64_decode($image_url);
 
-$sticker_values = explode(',', $_POST['sticker']);
-$sticker = file_get_contents('stickers/' . $sticker_values[0]);
-$h_offset = $sticker_values[1];
-$v_offset = $sticker_values[2];
-$width = $sticker_values[3];
-$height = $sticker_values[4];
+	$sticker_values = explode(',', $_POST['sticker']);
+	$sticker = file_get_contents('stickers/' . $sticker_values[0]);
+	$h_offset = $sticker_values[1];
+	$v_offset = $sticker_values[2];
+	$width = $sticker_values[3];
+	$height = $sticker_values[4];
 
-$image = imagecreatefromstring($image);
-imageflip($image, IMG_FLIP_HORIZONTAL);
-$sticker = imagecreatefromstring($sticker);
-$sticker = imagescale($sticker, $width, $height);
+	$image = imagecreatefromstring($image);
+	imageflip($image, IMG_FLIP_HORIZONTAL);
+	$sticker = imagecreatefromstring($sticker);
+	$sticker = imagescale($sticker, $width, $height);
 
-// Copy and merge
-imagecopy($image, $sticker, $h_offset, $v_offset, 0, 0, $width, $height);
+	// Copy and merge
+	imagecopy($image, $sticker, $h_offset, $v_offset, 0, 0, $width, $height);
 
-ob_start();
-imagejpeg($image);
-$image = ob_get_clean();
+	ob_start();
+	imagejpeg($image);
+	$image = ob_get_clean();
 
-$image = base64_encode($image);
+	$image = base64_encode($image);
 
-$sql = "INSERT INTO `images` (`user_id`, `image_data`) VALUES (?, ?)";
-$stmt = $pdo->prepare($sql);
-$stmt->execute([$_SESSION['user_id'], $image]);
+	$sql = "INSERT INTO `images` (`user_id`, `image_data`) VALUES (?, ?)";
+	$stmt = $pdo->prepare($sql);
+	$stmt->execute([$_SESSION['user_id'], $image]);
 
-echo "data:image/jpeg;base64," . $image;
-
-}	else {
+	echo "data:image/jpeg;base64," . $image;
+} else {
 	echo "ERROR!";
 }
-?>
